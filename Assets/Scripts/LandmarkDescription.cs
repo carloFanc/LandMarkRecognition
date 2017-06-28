9d6e33ae818fa0b43791245ef8a483f9dfd5c297
DESKTOP-8O3U0EB\carlo <fanciullicarlo@gmail.com>
2017-06-28 20:02:45 +0200

    commit|&|push

---
+++ Assets/Scripts/LandmarkDescription.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
+
+public class LandmarkDescription : MonoBehaviour
+{
+    private Text txtRef;
+    Text text;
+
+    void OnEnable()
+    {
+        text = GameObject.Find("LandMarkName").GetComponent<Text>();
+        txtRef = GetComponent<Text>();
+        txtRef.text = ""; 
+        StartCoroutine(GetText());  
+    }
+
+     
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=1&titles="+ text.text );
+        yield return www.Send();
+
+        if (www.isError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        { 
+            JObject o = JObject.Parse(www.downloadHandler.text);   
+           
+            txtRef.text = "\n" + o["query"]["pages"].First.Last.Last.Last;
+
+        }
+    }
+} 
---
+++ Assets/Scripts/LandmarkHandling.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.VR.WSA.WebCam; 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine.UI; 
+
+ 
+public class LandmarkHandling : MonoBehaviour 
+{
+    Text text;
+    GameObject menuPanels;
+    string mon;
+    Resolution cameraResolution;
+    PhotoCapture photoCapt; 
+    public string m_apiKey; 
+    public GameObject panel;
+    public GameObject PrimaryInterface;
+    AudioSource audiop;
+
+    public void Start()
+    {
+        panel.SetActive(false);
+        text = GameObject.Find("LandMarkName").GetComponent<Text>(); 
+        text.text = ""; 
+        PrimaryInterface.SetActive(true);
+        audiop = GetComponent<AudioSource>();
+    } 
+    public void StartTheFunction()
+    {
+        
+        audiop.Play();
+        PrimaryInterface.SetActive(false); 
+        cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject)
+        {
+            photoCapt = captureObject;
+             
+            CameraParameters cameraPar = new CameraParameters();
+            cameraPar.hologramOpacity = 0.0f;
+            cameraPar.cameraResolutionWidth = cameraResolution.width;
+            cameraPar.cameraResolutionHeight = cameraResolution.height;
+            cameraPar.pixelFormat = CapturePixelFormat.JPEG;
+
+            photoCapt.StartPhotoModeAsync(cameraPar, delegate (PhotoCapture.PhotoCaptureResult result)
+            {
+                PictureShoot();
+            });
+        });
+    } 
+
+    void OnDestroy()
+    {
+        if(photoCapt != null) {
+            photoCapt.StopPhotoModeAsync(
+          delegate (PhotoCapture.PhotoCaptureResult res)
+          {
+              photoCapt.Dispose();
+              photoCapt = null; 
+          }
+        );
+        }
+    }
+
+     
+    void PictureShoot()
+    {
+
+        if (photoCapt != null)
+        {
+            photoCapt.TakePhotoAsync(delegate (PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+            {
+                List<byte> buffer = new List<byte>();
+                Matrix4x4 cameraToWorldMatrix;
+                photoCaptureFrame.CopyRawImageDataIntoBuffer(buffer);
+
+
+                if (!photoCaptureFrame.TryGetCameraToWorldMatrix(out cameraToWorldMatrix))
+                {
+                    return;
+                }  
+                StartCoroutine(PhotoUploading(buffer.ToArray()));
+            });
+        }
+    }
+     
+    UnityWebRequest RequestCreation(byte[] photo)
+    {
+
+        DownloadHandler download = new DownloadHandlerBuffer();
+
+        string base64image = Convert.ToBase64String(photo);
+        string json = "{\"requests\": [{\"image\": {\"content\": \"" + base64image + "\"},\"features\": [{\"type\": \"LANDMARK_DETECTION\",\"maxResults\": 5}]}]}";
+        byte[] content = Encoding.UTF8.GetBytes(json);
+        UploadHandler upload = new UploadHandlerRaw(content);
+        string url = "https://vision.googleapis.com/v1/images:annotate?key=" + m_apiKey;
+        UnityWebRequest www = new UnityWebRequest(url, "POST", download, upload);
+        www.SetRequestHeader("Content-Type", "application/json");
+
+        return www;
+         
+    }
+
+    IEnumerator PhotoUploading(byte[] photo)
+    {
+        using (UnityWebRequest www = RequestCreation(photo))
+        {
+             
+            yield return www.Send();
+
+            if (www.isError)
+            {
+                Debug.Log("errore" + www.error);
+            }
+            else
+            { 
+                string jsonString = www.downloadHandler.text;
+              
+                JsonTextReader reader = new JsonTextReader(new StringReader(jsonString));
+
+                int count = 0;
+                mon = null;
+                while (reader.Read())
+                {
+                    if (reader.Value != null)
+                    {
+                        count++;
+                        if (count == 6)
+                            mon = reader.Value.ToString(); 
+                    }
+                } 
+
+                if (mon != null) { 
+                               text.text = mon;
+                            }else
+                                {
+                                text.text = "Not found, Sorry. Retry.";
+                                 }
+                if (!text.text.Equals("Not found, Sorry. Retry.")) { 
+                    OnDestroy();
+                    StartCoroutine(LateCall());
+                }
+            }
+        }
+    }
+    IEnumerator LateCall()
+    {
+        yield return new WaitForSeconds(2);
+        panel.SetActive(true); 
+    } 
+      
+}
+ 
